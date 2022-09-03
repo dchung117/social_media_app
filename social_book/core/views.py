432ee17a7ced4b3c809feb6c6d3b3158cacd79da
David <dchung0330@gmail.com>
2022-09-03 17:29:03 -0400

    created url route for home page.

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+from . import views
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Welcome to Social Book</h1>")

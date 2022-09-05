d413ace82970e25df1b287b3a57090dc4bf46596
David <dchung0330@gmail.com>
2022-09-05 16:00:26 -0400

    rendering static files.

--- social_book/social_book/settings.py
+++ social_book/social_book/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from importlib.resources import path
 from pathlib import Path
 from .secrets import SECRET_KEY
 
@@ -114,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = Path(BASE_DIR) / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"] # directory where static files reside
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
